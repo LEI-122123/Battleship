32474170ec8bb064dd4571a789cd95c0d549bbc5
LEI-106090 <tmcas2@iscte-iul.pt>
2025-11-09 17:18:06 +0000

    Add tests for Position class

--- src/test/java/iscteiul/ista/PositionCompassTest.java
+++ src/test/java/iscteiul/ista/PositionCompassTest.java
@@ -1,22 +1,20 @@
 package iscteiul.ista.battleship;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 
-import static iscteiul.ista.battleship.Compass.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionCompassTest
 {
-    private Position pos1;
-    private Position pos2;
+//    private Position pos1;
+//    private Position pos2;
 
     @BeforeEach
     void setUp()
     {
-        pos1 = new Position( 0, 0 );
-        pos2 = new Position( 0, 1 );
+        //
     }
 
     @AfterEach
@@ -61,13 +59,61 @@ public class PositionCompassTest
 
     //  Position Class Tests
 
-    @DisplayName( "Dummy shoot itself test" )
     @Test
-    @Disabled
-    void testShoot()
-    {
-        //ship.shoot( pos1 );
-        assertTrue( pos1.isHit() );
-        assertFalse( pos2.isHit() );
+    @DisplayName("Test constructor and getters")
+    void testConstructorAndGetters() {
+        Position pos = new Position(2, 3);
+        assertEquals(2, pos.getRow(), "Row should be 2");
+        assertEquals(3, pos.getColumn(), "Column should be 3");
+        assertFalse(pos.isOccupied(), "Position should not be occupied initially");
+        assertFalse(pos.isHit(), "Position should not be hit initially");
     }
+
+    @Test
+    @DisplayName("Test occupy and shoot methods")
+    void testOccupyAndShoot() {
+        Position pos = new Position(1, 1);
+        pos.occupy();
+        assertTrue(pos.isOccupied(), "Position should be occupied after occupy()");
+        pos.shoot();
+        assertTrue(pos.isHit(), "Position should be hit after shoot()");
+    }
+
+    @ParameterizedTest(name = "Position ({0},{1}) is adjacent to ({2},{3}) -> {4}")
+    @CsvSource({
+            "0,0,0,1,true",
+            "0,0,1,1,true",
+            "0,0,1,0,true",
+            "0,0,2,2,false",
+            "3,3,4,5,false"
+    })
+    @DisplayName("Test adjacency between positions")
+    void testIsAdjacentTo(int row1, int col1, int row2, int col2, boolean expected) {
+        Position pos1 = new Position(row1, col1);
+        Position pos2 = new Position(row2, col2);
+        assertEquals(expected, pos1.isAdjacentTo(pos2));
+    }
+
+    @Test
+    @DisplayName("Test equals and hashCode")
+    void testEqualsAndHashCode() {
+        Position pos1 = new Position(2, 3);
+        Position pos2 = new Position(2, 3);
+        Position pos3 = new Position(3, 2);
+
+        // equals
+        assertEquals(pos1, pos2, "Positions with same row and column should be equal");
+        assertNotEquals(pos1, pos3, "Positions with different row/column should not be equal");
+
+        // hashCode
+        assertEquals(pos1.hashCode(), pos2.hashCode(), "Hashcodes should match for equal positions");
+    }
+
+    @Test
+    @DisplayName("Test Position \"toString\" method")
+    void testPositionToString() {
+        Position pos = new Position(5, 7);
+        assertEquals("Linha = 5 Coluna = 7", pos.toString());
+    }
+
 }
